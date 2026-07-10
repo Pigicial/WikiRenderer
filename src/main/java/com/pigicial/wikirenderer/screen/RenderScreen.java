066ba86c9158c88e9ac23709f59b2aea83d3b1ee
KoalaMauve <koala@mauve.dev>
2026-07-10 14:21:33 -0700

    feat: add a cutstom tEXt metadata field with the minecraft texture_id for heads

--- src/main/java/com/pigicial/wikirenderer/screen/RenderScreen.java
+++ src/main/java/com/pigicial/wikirenderer/screen/RenderScreen.java
@@ -700,7 +700,7 @@ public class RenderScreen extends BaseOwoScreen<FlowLayout> {
         }
 
         RenderableDispatcher.drawIntoImage(this, this.renderable, tickDelta, this.getTimeSinceCreationMs(), renderable.getExportResolution(), renderable.shouldCrop(), dataConsumer)
-                .thenCompose(img -> FileIO.saveImage(img, exportPath).whenComplete((f, t) -> img.close()))
+                .thenCompose(img -> FileIO.saveImage(img, exportPath, this.renderable.getPngTextMetadata()).whenComplete((f, t) -> img.close()))
                 .whenComplete((imageFile, throwable) -> {
                     capturing = false;
                     if (throwable != null) {
