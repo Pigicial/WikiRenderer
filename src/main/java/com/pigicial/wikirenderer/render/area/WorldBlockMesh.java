c446119e06ea9489a72154bf9f87125981c0c4cb
Pigicial <72773248+Pigicial@users.noreply.github.com>
2026-08-13 13:57:37 -0700

    fix many custom block entity types not rendering at all

--- src/main/java/com/pigicial/wikirenderer/render/area/WorldBlockMesh.java
+++ src/main/java/com/pigicial/wikirenderer/render/area/WorldBlockMesh.java
@@ -417,6 +417,10 @@ public class WorldBlockMesh {
                 }
 
                 BlockEntityRenderState state = blockEntityDispatcher.tryExtractRenderState(entity, tickDelta, breakProgress, true);
+                if (state == null) {
+                    state = blockEntityDispatcher.tryExtractRenderState(entity, tickDelta, breakProgress, false);
+                }
+
                 if (state instanceof BeaconRenderState && AreaPropertyBundle.INSTANCE.hideBeaconBeams.get()) {
                     return;
                 }
